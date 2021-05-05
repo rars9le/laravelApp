9b61eefbc363648943ef90310c4d3dc8a2b6c081
rars9le <le9240@gmail.com>
2021-05-05 15:59:24 +0900

    person model add global scope age over 20

--- backend/app/Models/Person.php
+++ backend/app/Models/Person.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
     use HasFactory;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function (Builder $builer) {
+            $builer->where('age', '>=', 20);
+        });
+    }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
 }
